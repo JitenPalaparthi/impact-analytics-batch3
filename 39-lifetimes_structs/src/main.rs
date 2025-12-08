f567ff4cb17f96f8b708a128c942b2f54aa6eea4
Jiten <jpalaparthi@gmail.com>
2025-12-08 12:37:54 +0530

    add more examples on lifetimes

---
+++ 39-lifetimes_structs/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let p1 = Person {
+        id: 101,
+        name: "Jiten".to_string(),
+        email: "Jitenp@Outlook.Com".to_string(),
+        address: "Trv".to_string(),
+    };
+
+    println!("{:#?}", p1);
+
+    
+    let name = "Jiten".to_string();
+    
+
+    let s1 = Student {
+        id: 101,
+        name: &name,
+        email: "Jitenp@Outlook.Com",
+        address: "Trv",
+    };
+
+    println!("{:#?}", s1);
+
+    // s1 and name dropped
+
+    let str1: String = "hello world".to_string();
+
+    let str2: &String = &str1;
+}
+
+#[derive(Debug)]
+struct Person {
+    // if there are no references in struct , we called as struct contsins owned data
+    id: i32,
+    name: String,
+    email: String,
+    address: String,
+}
+// it strores in stack , but some data is in heap
+
+#[derive(Debug)]
+struct Student<'a> {
+    // if there are no references in struct , we called as struct contsins owned data
+    id: i32,
+    name: &'a String,
+    email: &'a str,
+    address: &'a str,
+}
+
+// in the address of Student --> "city:Bangalore,state:karnataka"
+// create a method , parse the address and return city and state as two separate strings
+
+// fn get_city_state()->(&str,&str)
