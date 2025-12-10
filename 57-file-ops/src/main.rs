6021c614c7eccf511ccfa2be5b88e2e2719a17f5
Jiten <jpalaparthi@gmail.com>
2025-12-10 14:51:02 +0530

    add more examples

---
+++ 57-file-ops/src/main.rs
@@ -0,0 +1,56 @@
+use std::{fs::File, io::Read};
+
+use std::thread;
+
+fn main() {
+    let handler1 = thread::spawn(|| -> String {
+        match File::open("data.txt") {
+            Ok(mut file) => {
+                let mut file_data = String::new();
+                match file.read_to_string(&mut file_data) {
+                    Ok(size) => {
+                        println!("File successfully read! --> file size-->{}", size);
+                        file_data
+                    }
+                    Err(e) => {
+                        println!("something went wrong: {}", e);
+                        "".to_string()
+                    }
+                }
+            }
+            Err(e) => panic!("err opening file"),
+        }
+    });
+
+    let handler2 = thread::spawn(|| match File::open("data.txt") {
+        Ok(mut file) => {
+            let mut file_data = String::new();
+            loop {
+                let mut buf: [u8; 1] = [0; 1];
+                match file.read(&mut buf[..]) {
+                    Ok(size) => {
+                        if size==0{
+                            println!("It has finished reading");
+                            break;
+                        }else{
+                            if buf[0]==b'\n'{
+
+                            }   
+                        }
+                    }
+                    Err(e) => {
+                        println!("It has finished reading-->{}",e);
+                        break;
+                    }
+                }
+            }
+        }
+        Err(e) => panic!("err opening file"),
+    });
+
+    let filedata = handler1.join().unwrap();
+    println!("{}", filedata);
+}
+
+// first , do the file read ,
+// iterate thru the file
