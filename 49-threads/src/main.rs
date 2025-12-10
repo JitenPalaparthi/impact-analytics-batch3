651711221f8e2c2b33116fc50075957ea94f46e6
Jiten <jpalaparthi@gmail.com>
2025-12-10 10:53:20 +0530

    add more examples on threads

---
+++ 49-threads/src/main.rs
@@ -0,0 +1,55 @@
+use core::time;
+use std::thread;
+
+fn main() {
+    let handle1 = thread::spawn(|| {
+        for i in 1..10 {
+            println!("thread-1 {}-->", i);
+            // thread::sleep(time::Duration::from_nanos(10));
+            // thread::sleep(time::Duration::from_millis(500)); // you are blocking the thread
+        }
+    });
+
+    let handle2 = thread::spawn(|| {
+        for i in 1..10 {
+            println!("thread-2 {}-->", i);
+            //thread::sleep(time::Duration::from_millis(500)); // you are blocking the thread
+        }
+    });
+    let mut sum = 0;
+
+    let handle3 = thread::spawn(move || -> i32 {
+        
+        for i in 1..=10 {
+            sum += i;
+        }
+        return sum;
+    });
+
+    println!("main has ended");
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+    let sum= handle3.join().unwrap();
+    println!("The sum of elements of thread-3: {}",sum);
+
+}
+
+// 16 core processor.
+// can run 16 threads at any point of time.
+
+// Multiplexing --> M:N
+// M is the OS thread
+// N is the hardware thread
+
+// each process has 1 thread by default
+
+// there is no hirarchy of threads.. according to OS
+
+// os scheduler --> preemptive threads for ex every 10 milli seconds (subjective)
+// each thread has stack that is 2mb (subjective to os setup or config)
+
+// main thread does not wait for any other thread to complete its execution
+
+// in multi thread application , it is not guaranteed to be exeucxuted in an order by design
+//
---
+++ 50-threads/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    
+
+}
+
+// loop of 10 
+
+// every time it should generate a rand number 100
+
+// for every rand number , take a saperate loop in side a separate threads from 1-- rand number ..
+// return the sum of numbers 
+
+//
