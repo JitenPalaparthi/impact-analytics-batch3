42f3c45f8fba6b4e4bd0e3cc3559516045f5df2a
Jiten <jpalaparthi@gmail.com>
2025-12-10 11:10:55 +0530

    add more examples on threads

--- 50-threads/src/main.rs
+++ 50-threads/src/main.rs
@@ -1,13 +1,33 @@
+use std::{fmt::format, thread};
+use tinyrand::{Rand, StdRand};
 fn main() {
-    
+    let range = 10;
+    let mut rand = StdRand::default();
+    let handle1 = thread::spawn(move || {
+        for i in 1..=range {
+            let lrange = rand.next_lim_u32(100);
+            let result = thread::spawn(move || -> String {
+                let mut sum = 0;
+                for i in 1..=lrange {
+                    sum += i;
+                }
+                return format!("sum from a thread of range:{}-->{}", lrange, sum);
+            })
+            .join()
+            .unwrap();
+            println!("{}", result);
+        }
+    });
 
+    handle1.join().unwrap();
+    println!("Done with main.. exiting")
 }
 
-// loop of 10 
+// loop of 10
 
 // every time it should generate a rand number 100
 
 // for every rand number , take a saperate loop in side a separate threads from 1-- rand number ..
-// return the sum of numbers 
+// return the sum of numbers
 
 //
